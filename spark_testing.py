80cf65887f117a6818dd297e94fc6c618f9975ef
Prabeernandi <prabeerveer77@gmail.com>
2023-01-02 17:34:23 +0530

    latest Changes

--- spark_testing.py
+++ spark_testing.py
@@ -1,9 +1,8 @@
-from numpy import False_
-import pyspark
-from pyspark.sql import Row
-from pyspark.sql import SparkSession
-import sys
 import os
+import sys
+
+from numpy import False_
+from pyspark.sql import SparkSession
 
 os.environ['HADOOP_HOME'] = "C:/hadoop-3.0.0"
 sys.path.append("C:/hadoop-3.0.0/bin")
@@ -12,8 +11,8 @@ spark = SparkSession \
     .builder \
     .appName("mongodbtest1") \
     .master('local') \
-    .config("spark.mongodb.input.uri", "mongodb://10.18.11.143:27017/vishwakarma_testing.pride") \
-    .config("spark.mongodb.output.uri", "mongodb://10.18.11.143:27017/vishwakarma_testing.pride") \
+    .config("spark.mongodb.input.uri", "mongodb://10.18.11.143:27017/vishwakarma_testing.otp") \
+    .config("spark.mongodb.output.uri", "mongodb://10.18.11.143:27017/vishwakarma_testing.otp") \
     .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1') \
     .getOrCreate()
 
@@ -22,6 +21,6 @@ books_tbl = spark.read \
     .load()
 books_table = books_tbl.createOrReplaceTempView("books_tbl")
 query1 = spark.sql("SELECT * FROM books_tbl")
-query1.show(20,truncate=False_)
 
-# print(books_tbl.first())
+query1.show(books_tbl.count(), truncate=False_)
+print(books_tbl.count())
